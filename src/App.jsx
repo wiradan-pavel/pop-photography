9710087985234cf3aa549a1c7ee546f4fb63ae09
Kaukenas Pavel <wiradan@bk.ru>
2023-11-15 10:01:35 +0300

    install dep, fonts + create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { Home } from './pages';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
---
+++ src/pages/ServiceTeen.jsx
@@ -0,0 +1,3 @@
+export default function ServiceTeen() {
+  return <div>ServiceTeen</div>;
+}
