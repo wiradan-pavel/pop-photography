a7fe028f61749947d656e96e71d75303de78b8fc
Kaukenas Pavel <wiradan@bk.ru>
2023-12-12 10:16:04 +0300

    complete Faq page

--- src/components/FaqContent/index.jsx
+++ src/components/FaqContent/index.jsx
@@ -1,34 +1,41 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { AccordionItem } from '../elements';
 import { faqContent } from '../../data/faq.data';
-import { quicklyListAni } from '../../data/animations.var'; 
+import { quicklyListAni } from '../../data/animations.var';
 
 import style from './style.module.scss';
 
 export default function FaqContent() {
+  const [search, setSearch] = useState('');
+
   return (
     <div className={style.wrapperWhite}>
       <div className={style.container}>
         <section className={style.section}>
           <div className={style.search}>
             <input
+              onChange={(e) => setSearch(e.target.value)}
               className={style.search__input}
               type="text"
               placeholder="Search Question, Keyword, Topics"
             />
-
-            <button className={style.btn}>Search</button>
           </div>
           <motion.div
             variants={quicklyListAni}
             initial="hidden"
-            whileInView="show"
-            viewport={{ once: true }}
+            animate="show"
             className={style.content}
           >
-            {faqContent.list.map((item) => (
-              <AccordionItem key={item.id} title={item.question} text={item.answer} />
-            ))}
+            {faqContent.list
+              .filter((item) => {
+                return search.toLowerCase() === ''
+                  ? item
+                  : item.question.toLowerCase().includes(search);
+              })
+              .map((item) => (
+                <AccordionItem key={item.id} title={item.question} text={item.answer} />
+              ))}
           </motion.div>
         </section>
       </div>
